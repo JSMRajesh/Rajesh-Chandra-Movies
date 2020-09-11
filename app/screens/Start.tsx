147e3ecee19b52c6d7a28b66503a840ae2f03c97
krchandra <rajesh.ksheersagar@skillsoft.com>
2020-09-11 15:09:13 +0530

    Matter Most code push

---
+++ app/screens/Start.tsx
@@ -0,0 +1,67 @@
+import {RouteProp, useRoute} from '@react-navigation/native';
+import React, {useState} from 'react';
+import {SafeAreaView, ScrollView, StatusBar, FlatList} from 'react-native';
+import {useValue} from 'react-native-redash';
+
+import Modal from '@components/Modal';
+import Movie from '@components/Movie';
+
+import type MovieType from '@app/types/Movie';
+import type PositionType from '@app/types/Position';
+
+interface ModalState {
+    movie: MovieType;
+    position: PositionType;
+}
+
+type StartParamList = {
+    Start: {
+        movies: Array<MovieType>;
+    };
+};
+
+type StartRoute = RouteProp<StartParamList, 'Start'>;
+
+const Start = () => {
+    const route = useRoute<StartRoute>();
+    const {movies} = route.params;
+    const activeMovieId = useValue<number>(-1);
+    const [modal, setModal] = useState<ModalState | null>(null);
+
+    const open = (index: number, movie: MovieType, position: PositionType) => {
+        activeMovieId.setValue(index);
+        setModal({movie, position});
+    };
+
+    const close = () => {
+        activeMovieId.setValue(-1);
+        setModal(null);
+    };
+    //Rajesh: Removed srollview and replaced with flatlist for prerfomance imporvement
+    return (
+        <>
+            <StatusBar barStyle="dark-content" />
+            <SafeAreaView>
+                <FlatList
+                    data={movies}
+                    renderItem={(item) => {
+                        return (
+                            <Movie
+                                activeMovieId={activeMovieId}
+                                key={item.item.id}
+                                index={item.index}
+                                movie={item.item}
+                                open={open}
+                            />
+                        );
+                    }}
+                    removeClippedSubviews={true}
+                    disableVirtualization={true}
+                />
+                {modal !== null && <Modal {...modal} close={close} />}
+            </SafeAreaView>
+        </>
+    );
+};
+
+export default Start;
